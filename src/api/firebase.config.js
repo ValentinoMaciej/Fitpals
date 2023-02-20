7d4d6f6e0e99f55d4ed57b6825be8f84f1da993b
Maciej Walentynowicz <106319323+ValentinoMaciej@users.noreply.github.com>
2023-02-20 21:15:23 +0100

    Added Final Project of ISA Graduation.

---
+++ src/api/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAnM7zT-q36kZE9AimNHR7KFxnP2AZe7wQ",
+  authDomain: "fitpals-efead.firebaseapp.com",
+  projectId: "fitpals-efead",
+  storageBucket: "fitpals-efead.appspot.com",
+  messagingSenderId: "202594272159",
+  appId: "1:202594272159:web:ef0cfcd02f05d9e08033aa",
+};
